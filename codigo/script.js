00ea418ba0bbec5d539352c9832f3366e61dda15
SMAC <santiagocosta.smac@gmail.com>
2026-01-11 17:59:24 +0000

    Create a api fetch on javascript
    Create a api fetch on javascript to frontend talk with backend

--- codigo/script.js
+++ codigo/script.js
@@ -1,3 +1,29 @@
 function enviarFormulario() {
-    //Botao de envio do formulario
+  const formulario = document.querySelector("form");
+  const nome = formulario.elements["nome"].value;
+  const email = formulario.elements["email"].value;
+  const assunto = formulario.elements["assunto"].value;
+  const mensagem = formulario.elements["mensagem"].value;
+  const URL = "https://site-fct.onrender.com/api/contacto";
+  fetch(URL, {
+    method: "post",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      nome: nome,
+      email: email,
+      assunto: assunto,
+      mensagem: mensagem,
+    }),
+  })
+    .then((resposta) => {
+      if (resposta.ok) {
+        return resposta.json();
+      } else {
+        console.log("Deu erro!");
+      }
+    })
+    .then((json) => console.log(json))
+    .catch((erro) => console.log(erro));
 }
